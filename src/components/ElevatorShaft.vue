d3dcb8304a30a7a0118a95000fdf545f0cb8ce34
IlyaBurch <ilyaburch@yandex.ru>
2024-04-04 18:31:49 +0300

    feature (ElevatorShaft) add movement to unit

--- src/components/ElevatorShaft.vue
+++ src/components/ElevatorShaft.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="shaft">
     <Button label="Открыть" @click="openDoors" />
-    <div v-for="item in floorsCount" :key="item.id">
+    <Button label="change" @click="changeFloor" />
+    <!-- <TransitionGroup name="fruit-list" tag="ul"> -->
+    <div v-for="item in floorsCount" :key="item.id" class="fruit-list-item">
       <ElevatorUnit :isOpened="isOpen" :floor="item.count" :isActive="item.active" />
     </div>
+    <!-- </TransitionGroup> -->
   </div>
 </template>
 
@@ -15,19 +18,24 @@ import Button from 'primevue/button'
 
 let isOpen = ref(false)
 
-const floorsCount = [
+const floorsCount = ref([
   { id: 0, count: 5, active: false },
   { id: 1, count: 4, active: false },
   { id: 2, count: 3, active: false },
   { id: 3, count: 2, active: false },
   { id: 4, count: 1, active: true }
-]
+])
 
 // const openDoors = computed(() => {
 //   return !isOpen.value
 // })
 const openDoors = function () {
   isOpen.value = !isOpen.value
+}
+
+const changeFloor = function () {
+  floorsCount.value[0].active = true
+  floorsCount.value[4].active = false
 }
 </script>
 
